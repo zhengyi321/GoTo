cd2f6cb7fa0b7bd613ddfdccae7dc55ddf73092c
zhyan <314643264@qq.com>
2017-02-26 19:14:09 +0800

    添加了短信验证的必须包，还未实现功能

--- app/src/main/java/tianhao/agoto/Activity/UserRegActivity.java
+++ app/src/main/java/tianhao/agoto/Activity/UserRegActivity.java
@@ -3,6 +3,8 @@ package tianhao.agoto.Activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -15,6 +17,7 @@ import rx.Observer;
 import tianhao.agoto.Bean.UserReg;
 import tianhao.agoto.NetWorks.UserSettingNetWorks;
 import tianhao.agoto.R;
+import tianhao.agoto.Utils.PhoneFormatCheckUtils;
 
 /**
  *
@@ -23,7 +26,9 @@ import tianhao.agoto.R;
  */
 
 public class UserRegActivity extends Activity {
-
+    /*验证码秒数*/
+    int second = 0;
+    /*验证码秒数*/
     /*手机号码*/
     @BindView(R.id.et_userreg_content_tel)
     EditText etUserRegContentTel;
@@ -43,6 +48,15 @@ public class UserRegActivity extends Activity {
 
     /*查看服务协议条款*/
 
+    /*获取验证码*/
+    @BindView(R.id.rly_userreg_content_smsec)
+    RelativeLayout rlyUserRegContentSMSec;
+    /*获取验证码*/
+    /*验证码倒计时秒数*/
+    @BindView(R.id.tv_userreg_content_smsec)
+    TextView tvUserRegContentSMSec;
+    /*验证码倒计时秒数*/
+
     /*回退上一页*/
     @BindView(R.id.rly_userreg_topbar_leftmenu_back)
     RelativeLayout rlyUserRegTopBarLeftMenuBack;
@@ -59,6 +73,78 @@ public class UserRegActivity extends Activity {
     private void init(){
         ButterKnife.bind(this);
     }
+
+    /*点击获取验证码*/
+    @OnClick(R.id.rly_userreg_content_smsec)
+    public void rlyUserRegContentSMSecOnclick(){
+        if(isPhoneNum()){
+             /*第一次点击倒计时*/
+            if(second == 0){
+                second = 60;
+                beginTimeing();
+            }else{
+                Toast.makeText(this,"亲，请耐心等待。。",Toast.LENGTH_LONG).show();
+            }
+        /*第一次点击倒计时*/
+        }else{
+            Toast.makeText(this,"亲，请输入正确的手机号码。。",Toast.LENGTH_LONG).show();
+        }
+
+    }
+    /*点击获取验证码*/
+    /*判断是否正确输入手机号码*/
+    private boolean isPhoneNum(){
+        String tel = etUserRegContentTel.getText().toString();
+        PhoneFormatCheckUtils phoneFormatCheckUtils = new PhoneFormatCheckUtils();
+        return  phoneFormatCheckUtils.isPhoneLegal(tel);
+    }
+    /*判断是否正确输入手机号码*/
+    /*多线程开始倒计时    http://blog.csdn.net/yhm2046/article/details/8213629/*/
+    private void beginTimeing(){
+        ThreadShow threadShow = new ThreadShow();
+        Thread thread = new Thread(threadShow);
+        thread.start();
+    }
+
+    // handler类接收数据
+    Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            if (msg.what == 1) {
+                if(second > 0) {
+                    tvUserRegContentSMSec.setText(Integer.toString(second));
+                    second--;
+                }else{
+                    tvUserRegContentSMSec.setText("");
+                    tvUserRegContentSMSec.setHint("获取验证码");
+                }
+                System.out.println("receive...."+second);
+            }
+        };
+    };
+
+    // 线程类
+    class ThreadShow implements Runnable {
+
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            while (second > 0) {
+                try {
+                    Thread.sleep(1000);
+                    Message msg = new Message();
+                    msg.what = 1;
+                    handler.sendMessage(msg);
+                    System.out.println("send...");
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    System.out.println("thread error...");
+                }
+            }
+        }
+    }
+
+    /*多线程开始倒计时*/
     /*注册提交点击事件*/
     @OnClick(R.id.rly_userreg_content_regsubmit)
     public void rlyUserRegContentRegSubmitOnclick(){
