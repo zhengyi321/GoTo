4b5fc2399424d271fb14e2b6bcbcf61b4a088be5
zhengyi321 <zhengyi321@126.com>
2017-02-27 22:19:21 +0800

    百度在线建议查询

--- app/src/main/java/tianhao/agoto/Activity/BaiduAddressSearchSuggestActivity.java
+++ app/src/main/java/tianhao/agoto/Activity/BaiduAddressSearchSuggestActivity.java
@@ -6,15 +6,24 @@ import android.support.v7.widget.RecyclerView;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 
+import com.baidu.mapapi.search.sug.OnGetSuggestionResultListener;
+import com.baidu.mapapi.search.sug.SuggestionResult;
+import com.baidu.mapapi.search.sug.SuggestionSearch;
+import com.baidu.mapapi.search.sug.SuggestionSearchOption;
+
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import tianhao.agoto.R;
 
 /**
+ *
+ * http://lbsyun.baidu.com/index.php?title=androidsdk/guide/retrieval
  * Created by admin on 2017/2/27.
  */
 
-public class BaiduAddressSearchSuggestActivity extends Activity{
+public class BaiduAddressSearchSuggestActivity extends Activity implements OnGetSuggestionResultListener {
 
     @BindView(R.id.rlv_baiduaddress_suggest_content)
     RecyclerView rlyBaiduAddressSuggestContent;
@@ -22,6 +31,7 @@ public class BaiduAddressSearchSuggestActivity extends Activity{
     RelativeLayout rlyBaiduAddressSearchSuggestTopBarBack;
     @BindView(R.id.et_baiduaddress_search_suggest_topbar_address)
     EditText etBaiduAddressSearchSuggestTopbarAddress;
+    private SuggestionSearch mSuggestionSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +44,31 @@ public class BaiduAddressSearchSuggestActivity extends Activity{
         /*initGlassBg();*/
         /*initTran();*/
 
+
+    }
+    private void initSuggestionSearch(){
+        mSuggestionSearch = SuggestionSearch.newInstance();
+        mSuggestionSearch.setOnGetSuggestionResultListener(this);
+    }
+    @Override
+    public void onGetSuggestionResult(SuggestionResult suggestionResult) {
+        if (suggestionResult == null || suggestionResult.getAllSuggestions() == null) {
+            return;
+            //未找到相关结果
+        }
+        //获取在线建议检索结果
+        List<SuggestionResult.SuggestionInfo> suggestionInfoList = suggestionResult.getAllSuggestions();
+    }
+    private void suggestionSearchOnLine(){
+        // 使用建议搜索服务获取建议列表，结果在onSuggestionResult()中更新
+        mSuggestionSearch.requestSuggestion((new SuggestionSearchOption())
+                .keyword("百度")
+                .city("北京"));
+    }
+
+    protected void onDestroy(){
+        mSuggestionSearch.destroy();
+        super.onDestroy();
 
     }
 }
