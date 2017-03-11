144adc0a8ec8dc110aa97b1d8ffb0723748807b5
zhyan <314643264@qq.com>
2017-03-11 17:13:30 +0800

    百度地图包

---
+++ app/src/main/java/tianhao/agoto/Common/Widget/SwipeCardView/SwipecardRecycleRView/ItemRemovedListener.java
@@ -0,0 +1,11 @@
+package tianhao.agoto.Common.Widget.SwipeCardView.SwipecardRecycleRView;
+
+/**
+ * Created by xingzhu on 2016/11/9.
+ */
+
+public interface ItemRemovedListener {
+    void onRightRemoved();
+
+    void onLeftRemoved();
+}
